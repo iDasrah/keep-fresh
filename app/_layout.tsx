97b085292dc59869983f241570c3589d78d3731b
Mathéo <dasrah.dev@gmail.com>
2025-10-12 22:42:21 +0200

    fix: fix status bar color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
 import { Stack } from "expo-router";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import {colors} from "@/constants/colors";
+import {StatusBar} from "expo-status-bar";
 
 export default function RootLayout() {
     return (
         <SafeAreaProvider>
             <SafeAreaView style={{flex: 1, backgroundColor: colors.black}} edges={['top', 'right', 'left']}>
+                <StatusBar style="light" />
                 <Stack screenOptions={{headerShown: false}}>
                     <Stack.Screen name="(tabs)" />
                 </Stack>
