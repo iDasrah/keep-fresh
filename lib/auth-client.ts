7485210c96798143e5625a7d4ac7d3af7b683456
lucien.schwob <lucien.schwob@etu.unistra.fr>
2025-10-17 01:03:44 +0200

    feat!: authentification app - api

---
+++ lib/auth-client.ts
@@ -0,0 +1,16 @@
+import { createAuthClient } from "better-auth/react"
+import {expoClient} from "@better-auth/expo/client";
+import * as SecureStore from "expo-secure-store";
+
+export const authClient = createAuthClient({
+    baseURL: process.env.EXPO_PUBLIC_API_URL,
+    basePath: 'auth',
+    trustedOrigins: ["mobile://"],
+    plugins: [
+        expoClient({
+            scheme: "mobile",
+            storagePrefix: "mobile",
+            storage: SecureStore,
+        })
+    ]
+})
