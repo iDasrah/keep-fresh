ade114a1e09701ab1fe3de9b2dc3366cc8eba9a9
lucien.schwob <lucien.schwob@etu.unistra.fr>
2025-10-29 21:23:29 +0100

    feat: nouveau composant permettant de scanner un produit

---
+++ app/(after-auth)/(app)/(tabs)/scan-product.tsx
@@ -0,0 +1,125 @@
+import {Alert, Button, StyleSheet, Text, View} from "react-native";
+import {useCallback, useRef, useState} from "react";
+import {CameraType, CameraView, useCameraPermissions} from "expo-camera";
+import {BarcodeScanningResult} from "expo-camera/src/Camera.types";
+import {api} from "@/lib/api";
+import {useFocusEffect, useRouter} from "expo-router";
+import {useApiMutation} from "@/lib/useApiMutation";
+import {CreateProductDto} from "@/generated-api";
+import {AxiosError} from "axios";
+import lang from "@/lib/lang";
+
+const ScanProduct = () => {
+  const [facing, _] = useState<CameraType>('back');
+  const camera = useRef<CameraView>(null);
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [permission, requestPermission] = useCameraPermissions();
+  const isProcessing = useRef(false);
+  const router = useRouter();
+
+  useFocusEffect(
+    useCallback(() => {
+      isProcessing.current = false;
+      camera.current?.resumePreview();
+      setIsActive(true);
+
+      return () => {
+        setIsActive(false);
+        camera.current?.pausePreview();
+        isProcessing.current = true;
+      };
+    }, [])
+  );
+
+  const getProductByBarcodeV1 = useApiMutation((data: string) => api.product.getProductByBarcodeV1(data));
+  const createProductV1 = useApiMutation((data: CreateProductDto) => api.product.createProductV1(data));
+
+  const handleBarcodeScanned = useCallback(async ({ data: barcode }: BarcodeScanningResult) => {
+    try {
+      if (isProcessing.current) return;
+      isProcessing.current = true;
+
+      camera.current?.pausePreview();
+      const product = await getProductByBarcodeV1.mutateAsync(barcode);
+      router.push(`/(after-auth)/(app)/add-item/${product.data.id}`);
+    } catch (_) {
+      try {
+        const product = await createProductV1.mutateAsync({
+          barcode,
+        });
+        router.push(`/(after-auth)/(app)/add-item/${product.data.id}`);
+      } catch (error: unknown) {
+        if (error instanceof AxiosError && error.response?.data?.message) {
+            Alert.alert('Error', error.response.data.message);
+            return;
+        }
+        Alert.alert('Error', lang.errors.generic);
+      }
+    }
+  }, []);
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+        <View style={styles.container}>
+          <Text style={styles.message}>We need your permission to show the camera</Text>
+          <Button onPress={requestPermission} title="grant permission" />
+        </View>
+    );
+  }
+
+  return (
+      <View style={styles.container}>
+        <CameraView
+            ref={camera}
+            active={isActive}
+            style={styles.camera}
+            facing={facing}
+            barcodeScannerSettings={{
+              barcodeTypes: [
+                'ean13',
+                'ean8',
+              ],
+            }}
+            onBarcodeScanned={handleBarcodeScanned}
+            autofocus={'on'}
+        />
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 64,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    width: '100%',
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
+
+export default ScanProduct;
